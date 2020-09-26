a1239b0b73aec9d2da5c90fe3aa3347886938704
ummehanirafa <71660284+ummehanirafa@users.noreply.github.com>
2020-09-26 15:44:14 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="./style.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+
+<body>
+  <section class="navbar">
+    <div class="link">
+      <ul>
+        <li><a href="">Home</a></li>
+        <li><a href="">About</a></li>
+        <li><a href="">Menu</a></li>
+        <li><a href="">Blog</a></li>
+        <li><a href="">Contact</a></li>
+      </ul>
+    </div>
+    <div class="social">
+      <i class="fa fa-facebook"></i>
+      <i class="fa fa-instagram"></i>
+      <i class="fa fa-twitter"></i>
+      <i class="fa fa-google-plus"></i>
+      <i class="span">+8801791107997</i>
+    </div>
+
+    <div class="text">
+      <p>Food for Ever</p>
+      <p>Bangladesh</p>
+    </div>
+
+
+    </div>
+  </section>
+
+
+
+  <section class="container">
+    <h3 class="text-center">Food Menu</h3>
+    <h1 class="text-center">Best Ever Burger</h1>
+    <div class="item ">
+      <div class="itemdetails flex">
+          <div class="img">
+              <img src="./b.png.jpg" alt="">
+          </div>
+          <div class="text">
+              <p>Burgur</p>
+              <p>Burgur is savory dish if italiano.</p>
+              <p>$120</p>
+          </div>
+      </div>
+
+
+
+
+    </div>
+  </section>
+
+
+
+</body>
+
+</html>
